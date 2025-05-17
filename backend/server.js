773446032e003c49d477fc1c5f90357e58ac7c31
Ogabek <mannonovogabek270@gmail.com>
2025-05-17 12:52:19 +0500

    remove all page

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const pool = require('./config/db'); // db.js faylini ulash
+const pool = require('./config/db'); 
 
 
 
@@ -10,22 +10,13 @@ const pool = require('./config/db'); // db.js faylini ulash
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 6666; // Portni .env faylidan yoki default 6666 dan olish
+const PORT = process.env.PORT || 6666;
 
 // Middlewarelarni ulash
-app.use(cors()); // CORS ni yoqish
-app.use(bodyParser.json()); // JSON formatidagi so'rovlarni tahlil qilish
+app.use(cors()); 
+app.use(bodyParser.json()); 
 
-// Routerlarni import qilish
-// Agar server ishga tushayotganda xato yuz bersa, routerlarni vaqtincha kommentariyaga oling
-const authRoutes = require('./routes/auth');
-const hallRoutes = require('./routes/halls');
-const bookingRoutes = require('./routes/bookings');
 
-// Routerlarni ulash
-app.use('/api/auth', authRoutes); // /api/auth yo'nalishi uchun authRoutes ni ishlatish
-app.use('/api/halls', hallRoutes); // /api/halls yo'nalishi uchun hallRoutes ni ishlatish
-app.use('/api/bookings', bookingRoutes); // /api/bookings yo'nalishi uchun bookingRoutes ni ishlatish
 
 // Serverni ishga tushirish
 app.listen(PORT, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import AddHallForm from './components/AddHallForm'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <AddHallForm />
+    </div>
   )
 }
